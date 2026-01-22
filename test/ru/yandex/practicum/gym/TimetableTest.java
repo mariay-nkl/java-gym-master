3283afdba8d987775c7cae930f16b39fe4e8beb9
marianik <nik1mari@yandex.ru>
2026-01-21 22:04:51 -0800

    Fix: исправлен класс TimetableTest

--- test/ru/yandex/practicum/gym/TimetableTest.java
+++ test/ru/yandex/practicum/gym/TimetableTest.java
@@ -1,6 +1,6 @@
 package ru.yandex.practicum.gym;
 
-import org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 import java.util.List;
 
