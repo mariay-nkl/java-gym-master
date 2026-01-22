d932cccfefcba5fcdb2fdff4d07d7f2e71e0fb20
marianik <nik1mari@yandex.ru>
2026-01-21 21:47:22 -0800

    Fix: исправлен класс DayOfWeek

--- src/ru/yandex/practicum/gym/DayOfWeek.java
+++ src/ru/yandex/practicum/gym/DayOfWeek.java
@@ -1,4 +1,4 @@
-git log --oneline --allpackage ru.yandex.practicum.gym;
+package ru.yandex.practicum.gym;
 
 public enum DayOfWeek {
     MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY
