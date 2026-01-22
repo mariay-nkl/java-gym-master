0720a88f887a7fce119e87ba65a2da2baff088c4
marianik <nik1mari@yandex.ru>
2026-01-21 21:34:19 -0800

    Fix: исправлен пробел перед '(' в конструкторе

--- src/ru/yandex/practicum/gym/DayOfWeek.java
+++ src/ru/yandex/practicum/gym/DayOfWeek.java
@@ -1,4 +1,4 @@
-package ru.yandex.practicum.gym;
+git log --oneline --allpackage ru.yandex.practicum.gym;
 
 public enum DayOfWeek {
     MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY
