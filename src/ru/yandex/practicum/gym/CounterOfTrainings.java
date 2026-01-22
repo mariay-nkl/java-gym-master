886a7a93cfb8feaa2b758d9b1979f682cfc1005c
marianik <nik1mari@yandex.ru>
2026-01-21 21:41:51 -0800

    Fix checkstyle: remove space before '(' in constructor

--- src/ru/yandex/practicum/gym/CounterOfTrainings.java
+++ src/ru/yandex/practicum/gym/CounterOfTrainings.java
@@ -4,7 +4,7 @@ public class CounterOfTrainings implements Comparable<CounterOfTrainings> {
     private Coach coach;
     private int count;
 
-    public CounterOfTrainings (Coach coach, int count) {
+    public CounterOfTrainings(Coach coach, int count) {
         this.coach = coach;
         this.count = count;
     }
